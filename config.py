5e4394a1c8a348924ffc0bab8d3f1c858d23798b
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-06-07 10:31:50 +0800

    update background.jpg

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import sys
 from os import path
 sys.path.append(path.dirname(__file__))
-# TODO smaller bg
 
 from src.game import Arkanoid
 
 GAME_SETUP = {
-    "game": Arkanoid,
-    # "dynamic_ml_clients":True
+    "game": Arkanoid
 }
